92bd56aad52219a6fd210f36995e0a36660dd463
Tsundere Chen <me@tsunderechen.io>
2020-11-25 00:00:17 +0800

    Separate function from main.c to linkedList.c

---
+++ linkedList.c
@@ -0,0 +1,102 @@
+#include "linkedList.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void add_entry(node_t **head, int new_value)
+{
+    node_t **indirect = head;
+
+    node_t *new_node = malloc(sizeof(node_t));
+    new_node->value = new_value;
+    new_node->next = NULL;
+
+    assert(new_node);
+    while (*indirect)
+        indirect = &(*indirect)->next;
+    *indirect = new_node;
+}
+
+node_t *find_entry(node_t *head, int value)
+{
+    node_t *current = head;
+    for (; current && current->value != value; current = current->next)
+        /* interate */;
+    return current;
+}
+
+void remove_entry(node_t **head, node_t *entry)
+{
+    node_t **indirect = head;
+
+    while ((*indirect) != entry)
+        indirect = &(*indirect)->next;
+
+    *indirect = entry->next;
+    free(entry);
+}
+
+void swap_pair(node_t **head) {
+  for (node_t **node = head; *node && (*node)->next;
+       node = &(*node)->next->next) {
+    node_t *tmp = *node;
+    *node = (*node)->next;
+    tmp->next = (*node)->next;
+    (*node)->next = tmp;
+  }
+  return;
+}
+
+void reverse(node_t **head) {
+  node_t **now = head;
+  node_t *cursor = NULL;
+  node_t *next = NULL;
+  while ((*now)) {
+    next = (*now)->next;
+    (*now)->next = cursor;
+    cursor = *now;
+    *now = next;
+  }
+  *now = cursor;
+  return;
+}
+
+void print_list(node_t *head)
+{
+    for (node_t *current = head; current; current = current->next)
+        printf("%d ", current->value);
+    printf("\n");
+}
+
+int get_list_length(node_t *head) {
+  int len = 0;
+  for (node_t *current = head; current; current = current->next)
+    len++;
+  return len;
+}
+
+node_t *node_finder(node_t *head, int position) {
+  node_t *now = head;
+  for (int i = 0; i < position; i++) {
+    now = now->next;
+  }
+  return now;
+}
+
+void swapValue(node_t *a, node_t *b) {
+  if (a == b)
+    return;
+  a->value ^= b->value;
+  b->value ^= a->value;
+  a->value ^= b->value;
+}
+
+void fisherYatesShuffle(node_t *head, int length) {
+  node_t *now = head;
+  for (int i = 0; i < length; i++) {
+    int randInt = i + (rand() % (length - i));
+    node_t *targetNode = node_finder(head, randInt);
+    swapValue(now, targetNode);
+    now = now->next;
+  }
+}
---
+++ linkedList.h
@@ -0,0 +1,15 @@
+typedef struct __node {
+    int value;
+    struct __node *next;
+} node_t;
+
+void add_entry(node_t **head, int new_value);
+node_t *find_entry(node_t *head, int value);
+void remove_entry(node_t **head, node_t *entry);
+void swap_pair(node_t **head);
+void reverse(node_t **head);
+void print_list(node_t *head);
+int get_list_length(node_t *head);
+node_t *node_finder(node_t *head, int position);
+void swapValue(node_t *a, node_t *b);
+void fisherYatesShuffle(node_t *head, int length);
